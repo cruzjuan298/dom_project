ebb873ea07d40fe1d4f21555c3bf421dede29d29
juanc <105023616+cruzjuan298@users.noreply.github.com>
2023-08-02 02:17:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Add your code to this file
+document.querySelector("h1").style["text-decoration"] = "underline"; 
+document.querySelector("body").style["background"] = "blue";
+document.querySelector("h2").style["textAlign"] = "center"; 
+
+let pokemon_images_array = document.getElementsByClassName("pokemon_images"); 
+for(let i = 0; i < pokemon_images_array.length; i++){ 
+    pokemon_images_array[i].style["border"]= "5px dashed yellow" 
+}; 
+
+document.querySelector("footer")
